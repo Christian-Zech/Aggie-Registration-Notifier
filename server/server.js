be79ece85a0afff1c78851e962b16e025427182c
Christian <74121448+Christian-Zech@users.noreply.github.com>
2024-09-06 20:21:37 -0500

    Cleaned up comments and added clarity to some functions

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ async function performOperation(operation) {
     let connection;
     try {
         // Use the connection string copied from the cloud console
-        // and stored in connstring.txt file from Step 2 of this tutorial
+        // and stored in connstring.txt fil
         connection = await oracledb.getConnection();
         result = await connection.execute(operation, {}, {
             autoCommit: true
@@ -68,7 +68,7 @@ app.get("/api", (req, res) => {
 
 app.post("/api", (req, res) => {
     const row = req.body.row;
-    const insertRowRequest = `INSERT INTO REACT.accounts_notify (email, class_name, class_sections, campus, ip_address) VALUES ('${row.email}', '${row.class_name.toUpperCase()}', '${row.class_sections}', '${row.campus}', '${req.headers['X-Forwarded-For']}')`; //Insert query
+    const insertRowRequest = `INSERT INTO REACT.accounts_notify (email, class_name, class_sections, campus) VALUES ('${row.email}', '${row.class_name.toUpperCase()}', '${row.class_sections}', '${row.campus}')`; //Insert query
     performOperation(insertRowRequest).then((result) => {
         res.send({ message: "Insert sucessfull!" });
     }).catch((error) => {
@@ -79,7 +79,7 @@ app.post("/api", (req, res) => {
 
 app.delete("/api", (req, res) => {
 
-
+    // verifies the delete request isn't malicious by cross checking it with info only the frontend would know
     performOperation(`SELECT * FROM REACT.accounts_notify WHERE id = ${req.query.id}`).then((result) => {
         if(req.query.email == result.rows[0][4]) {
             const deleteRowRequest = `DELETE FROM REACT.accounts_notify WHERE id = ${req.query.id}`; //Delete query
